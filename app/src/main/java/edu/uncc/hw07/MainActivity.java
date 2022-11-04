8e99c98339c2d16850ed5ebb90ae2c4e49066055
Stephanie <slee185@uncc.edu>
2022-11-04 13:26:12 -0400

    initial commit

---
+++ app/src/main/java/edu/uncc/hw07/MainActivity.java
@@ -0,0 +1,18 @@
+package edu.uncc.hw07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.rootView, new LoginFragment())
+                .commit();
+    }
+}
